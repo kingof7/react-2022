adef40ffe09d14569cf350399b59a30a5990c9b1
vlfl0 <vlfl0@naver.com>
2023-10-07 16:04:34 +0900

    for문으로 태그만들때 key 속성 줘서 유니크하게 해야함

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function Nav(props) {
 const lis = []
 for(let i=0; i<props.topics.length; i++) {
   let t = props.topics[i];
-  lis.push(<li>{t.title}</li>)
+  lis.push(<li key={t.id}><a href={'/read/'+t.id}>{t.title}</a></li>)
 }
   return <nav>
     <ol>
